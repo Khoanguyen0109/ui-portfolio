66b3e85b27d0402c7be0373b36ec9e899e1c1cb9
Khoa Nguyen <khoanguyen0109.99@gmail.com>
2025-01-20 23:27:11 +0700

    update tiktok

--- src/data.js
+++ src/data.js
@@ -24,22 +24,31 @@ import design from "./assets/design/Content.jpg";
 import cinethumbe from "./assets/cine/cinethumb.jpg";
 import cine from "./assets/cine/cine.jpg";
 import remithumb from "./assets/remitano/thumb.jpg";
-import remidetail from "./assets/remitano/detail.jpg"
-import lendthumb from "./assets/lend/lend.jpg"
-import detail from "./assets/lend/detail.jpg"
+import remidetail from "./assets/remitano/detail.jpg";
+import lendthumb from "./assets/lend/lend.jpg";
+import detail from "./assets/lend/detail.jpg";
 
+import noBee1 from "./assets/newNobee/1.jpg";
+import noBee2 from "./assets/newNobee/2.jpg";
+import noBee3 from "./assets/newNobee/3.jpg";
+import noBee4 from "./assets/newNobee/4.jpg";
+import noBee5 from "./assets/newNobee/5.jpg";
+import noBee6 from "./assets/newNobee/6.jpg";
+import noBee7 from "./assets/newNobee/7.jpg";
+import noBee8 from "./assets/newNobee/8.jpg";
+import noBeeThumb from "./assets/newNobee/Thumnail.jpg";
 
-import noBee1 from "./assets/newNobee/1.jpg"
-import noBee2 from "./assets/newNobee/2.jpg"
-import noBee3 from "./assets/newNobee/3.jpg"
-import noBee4 from "./assets/newNobee/4.jpg"
-import noBee5 from "./assets/newNobee/5.jpg"
-import noBee6 from "./assets/newNobee/6.jpg"
-import noBee7 from "./assets/newNobee/7.jpg"
-import noBee8 from "./assets/newNobee/8.jpg"
-import noBeeThumb from "./assets/newNobee/Thumnail.jpg"
-
-
+import tiktok1 from "./assets/newTiktok/Thumbnail.jpg";
+import tiktok2 from "./assets/newTiktok/2.jpg";
+import tiktok3 from "./assets/newTiktok/3.jpg";
+import tiktok4 from "./assets/newTiktok/4.jpg";
+import tiktok5 from "./assets/newTiktok/5.jpg";
+import tiktok6 from "./assets/newTiktok/6.jpg";
+import tiktok7 from "./assets/newTiktok/7.jpg";
+import tiktok8 from "./assets/newTiktok/8.jpg";
+import tiktok9 from "./assets/newTiktok/9.jpg";
+import tiktok10 from "./assets/newTiktok/10.jpg";
+import tiktok11 from "./assets/newTiktok/11.jpg";
 
 const projects = [
   {
@@ -52,61 +61,82 @@ const projects = [
       {
         id: 1,
         url: noBee1,
-        // width: "1320px",
-        // height: "22186px",
       },
       {
         id: 2,
         url: noBee2,
-        // width: "1320px",
-        // height: "22186px",
       },
       {
         id: 3,
         url: noBee3,
-        // width: "1320px",
-        // height: "22186px",
       },
       {
         id: 4,
         url: noBee4,
-
       },
       {
         id: 5,
         url: noBee5,
- 
       },
       {
         id: 6,
         url: noBee6,
-        // width: "1320px",
-        // height: "22186px",
       },
       {
         id: 7,
         url: noBee7,
-
       },
       {
         id: 8,
         url: noBee8,
-
       },
-
     ],
   },
   {
     id: 242,
     name: "Tik tok",
-    thumbnail: tiktokthumb,
+    thumbnail: tiktok1,
     description: " ",
     images: [
       {
+        id: 1,
+        url: tiktok2,
+      },
+      {
         id: 2,
-        url: tiktok,
-        width: "1320px",
-        height: "13925px",
+        url: tiktok3,
+      },
+      {
+        id: 3,
+        url: tiktok4,
+      },
+      {
+        id: 4,
+        url: tiktok5,
+      },
+      {
+        id: 5,
+        url: tiktok6,
+      },
+      {
+        id: 6,
+        url: tiktok7,
+      },
+      {
+        id: 7,
+        url: tiktok8,
+      },
+      {
+        id: 8,
+        url: tiktok9,
+      },
+      {
+        id: 9,
+        url: tiktok10,
+      },
+      {
+        id: 10,
+        url: tiktok11,
       },
     ],
   },
