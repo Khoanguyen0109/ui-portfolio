9c70c5bf19ecd7734b1ca06c13394913bf885197
Khoa Nguyen <khoanguyen0109.99@gmail.com>
2025-01-20 23:43:08 +0700

    update back group

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Banner } from '../components/Banner';
-import { Contact } from '../components/Contact';
-import { Footer } from '../components/Footer';
-import { NavBar } from '../components/NavBar';
-import { Projects } from '../components/Projects';
-import { Skills } from '../components/Skills';
-import { Tools } from '../components/Tool';
-import { Intro } from '../components/Intro';
-
+import React from "react";
+import { Banner } from "../components/Banner";
+import { Contact } from "../components/Contact";
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBar";
+import { Projects } from "../components/Projects";
+import { Skills } from "../components/Skills";
+import { Tools } from "../components/Tool";
+import { Intro } from "../components/Intro";
+import hero from "../assets/BACK ground.jpg";
 function Home() {
-  console.log('home');
+  console.log("home");
   return (
     <div>
-      <Banner />
+      {/* <Banner /> */}
+      <img src={hero} alt="hero" />
       {/* <Intro/> */}
       {/* <Skills />
       
