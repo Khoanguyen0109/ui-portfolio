0b38fe0a082ec8719303d128fafb72365ac14044
Khoa nguyen <khn6352@yum.com>
2026-06-30 14:17:10 +0700

    fix

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -6,7 +6,6 @@ import { ImageGallery } from '../components/ImageGallery';
 import { CaseStudyTestGenerator } from '../components/CaseStudyTestGenerator';
 import { CaseStudyImportTestCase } from '../components/CaseStudyImportTestCase';
 import { CaseStudyGetNow } from '../components/CaseStudyGetNow';
-import { CaseStudyGetNowSlides } from '../components/CaseStudyGetNowSlides';
 import NotFound from './NotFound';
 
 function Detail() {
@@ -18,23 +17,19 @@ function Detail() {
   const isFullCaseStudy = project.caseStudyType === 'full';
   const isImportCaseStudy = project.caseStudyType === 'import-case-study';
   const isGetNow = project.caseStudyType === 'getnow';
-  const isGetNowSlides = project.caseStudyType === 'getnow-slides';
-
   return (
     <>
-      {!isGetNow && !isGetNowSlides && <NavBar />}
+      {!isGetNow && <NavBar />}
       {isFullCaseStudy ? (
         <CaseStudyTestGenerator project={project} />
       ) : isImportCaseStudy ? (
         <CaseStudyImportTestCase project={project} />
       ) : isGetNow ? (
         <CaseStudyGetNow project={project} />
-      ) : isGetNowSlides ? (
-        <CaseStudyGetNowSlides project={project} />
       ) : (
         <ImageGallery project={project} />
       )}
-      {!isGetNow && !isGetNowSlides && <Footer />}
+      {!isGetNow && <Footer />}
     </>
   );
 }
